b05b564b4a3375b823ee837a8493e213df1432c0
luhenchang <1276998208@qq.com>
2017-08-23 23:38:14 +0800

    第二次提交

--- app/src/main/java/com/example/ls/lsn7_materialdesign_slidingmenu_drawerlayout/MainActivity.java
+++ app/src/main/java/com/example/ls/lsn7_materialdesign_slidingmenu_drawerlayout/MainActivity.java
@@ -26,46 +26,7 @@ public class MainActivity extends ActionBarActivity {
         drawerLayout = (DrawerLayout) findViewById(R.id.drawable_layout);
         ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.drawer_open, R.string.drawer_close);
         actionBarDrawerToggle.syncState();
-
         drawerLayout.addDrawerListener(drawerableLinstner);
-        /*drawerLayout.setDrawerListener(new DrawerLayout.DrawerListener() {
-            *//**
-         * @param drawerView 这个是抽屉里面移动的子布局。
-         * @param slideOffset 这个是抽屉里面移动的距离比例0-1
-         *//*
-            @Override
-            public void onDrawerSlide(View drawerView, float slideOffset){
-                View zhong_view = drawerLayout.findViewById(R.id.neirou);
-                View rightview = drawerView;
-                float scale = 1 - slideOffset;//1-0
-                float leftScale = (float) (1 - 0.3 * scale);//1-(0.3-0)=0.7->1
-                float rightScale = (float) (0.7f + 0.3 * scale);//0.7+(0.3-0)=1->0.7
-                rightview.setScaleX(leftScale);
-                rightview.setScaleY(leftScale);
-
-                zhong_view.setScaleX(rightScale);
-                zhong_view.setScaleY(rightScale);
-                zhong_view.setTranslationX(rightview.getMeasuredWidth() * (1 - scale));//0-width;
-
-            }
-
-            @Override
-            public void onDrawerOpened(View drawerView) {
-
-            }
-
-            @Override
-            public void onDrawerClosed(View drawerView) {
-
-            }
-
-            @Override
-            public void onDrawerStateChanged(int newState) {
-
-            }
-        });
-
-*/
     }
 
     @Override
@@ -77,16 +38,25 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        drawerLayout.removeDrawerListener(drawerableLinstner);
+        if(drawerableLinstner!=null) {
+            drawerLayout.removeDrawerListener(drawerableLinstner);
+        }
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        if(drawerableLinstner!=null) {
+            drawerLayout.removeDrawerListener(drawerableLinstner);
+        }
     }
 
     class MyDrawerListener implements DrawerLayout.DrawerListener {
 
+        /**
+         * @param drawerView 这个是抽屉里面移动的子布局。
+         * @param slideOffset 这个是抽屉里面移动的距离比例0-1
+         */
         @Override
         public void onDrawerSlide(View drawerView, float slideOffset) {
             //中间的布局
